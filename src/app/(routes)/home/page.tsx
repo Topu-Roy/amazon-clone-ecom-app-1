748ca80b8ac52822d13d374ad84f6303abc39fc8
Topu-Roy <topuroy143333@gmail.com>
2023-08-03 16:32:02 +0600

    price text nicely formatted now

--- src/app/(routes)/home/page.tsx
+++ src/app/(routes)/home/page.tsx
@@ -20,47 +20,65 @@ async function HomePage() {
         </div>
 
         <div className="w-ful grid grid-cols-5 gap-4">
-          {products.map((item: ProductType) => (
-            <div className="h-80 p-2 bg-white rounded-lg flex flex-col justify-between hover:shadow-2xl hover:cursor-pointer">
-              <div>
-                <div className="flex h-44 w-auto overflow-hidden items-center justify-center mx-auto rounded-lg">
-                  <Image
-                    alt={item.title}
-                    src={item.image}
-                    width={500}
-                    height={500}
-                    className="bg-contain"
-                  />
-                </div>
+          {products.map((item: ProductType) => {
+            const splitPrice = item.price.toString().split(".");
 
-                <p className="pt-2 text-lg font-medium">
-                  {item.title.length > 25
-                    ? item.title.slice(0, 25) + "..."
-                    : item.title}
-                </p>
+            const decimalPrice =
+              splitPrice[1] == null || 0 || undefined ? "00" : splitPrice[1];
+            const updatedDecimalPrice =
+              decimalPrice.length == 1 ? "00" : decimalPrice;
 
-                <p className="text-sm text-gray-700">
-                  {item.description.length > 60
-                    ? item.description.slice(0, 60) + "..."
-                    : item.description}
-                </p>
-              </div>
+            return (
+              <div className="h-80 p-2 bg-white rounded-lg flex flex-col justify-between hover:shadow-2xl hover:cursor-pointer">
+                <div>
+                  <div className="flex h-44 w-auto overflow-hidden items-center justify-center mx-auto rounded-lg">
+                    <Image
+                      alt={item.title}
+                      src={item.image}
+                      width={500}
+                      height={500}
+                      className="bg-contain"
+                    />
+                  </div>
 
-              <div className="flex justify-between items-center">
-                <div className="flex items-center justify-center gap-1">
-                  <MdOutlineStarRate size={25} color={"#d8db2e"} />
-                  <p>{item.rating.rate}</p>
-                  <p className="text-gray-600 text-sm">
-                    {" "}
-                    - ({item.rating.count})
+                  <p className="pt-2 text-lg font-medium">
+                    {item.title.length > 25
+                      ? item.title.slice(0, 25) + "..."
+                      : item.title}
+                  </p>
+
+                  <p className="text-sm text-gray-700">
+                    {item.description.length > 60
+                      ? item.description.slice(0, 60) + "..."
+                      : item.description}
                   </p>
                 </div>
-                <div className="flex items-center justify-between gap-1">
-                  <p>${item.price}</p>
+
+                <div className="flex justify-between items-center">
+                  <div className="flex items-center justify-center gap-1">
+                    <MdOutlineStarRate size={20} color={"#d8db2e"} />
+                    <p>{item.rating.rate}</p>
+                    <p className="text-gray-600 text-sm">
+                      - ({item.rating.count})
+                    </p>
+                  </div>
+                  <div className="flex items-center justify-center">
+                    <p className="flex justify-center text-gray-900">
+                      <span className="flex justify-center items-start align-text-top text-sm">
+                        $
+                      </span>
+                      <span className="flex justify-center items-center align-top text-xl font-semibold">
+                        {item.price.toFixed(0)}
+                      </span>
+                      <span className="text-sm flex justify-center items-end text-gray-700 align-text-bottom">
+                        {updatedDecimalPrice}
+                      </span>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </section>
     </>
