157d088a1f07deea3c88e062bb2c32f046936513
Topu-Roy <topuroy143333@gmail.com>
2023-08-04 23:45:48 +0600

    Improved - Cart page

--- src/app/(routes)/cart/page.tsx
+++ src/app/(routes)/cart/page.tsx
@@ -1,15 +1,23 @@
 "use client";
+import { Headline, ProductCart } from "@/components";
 import { useCartStore } from "@/zustand/cart";
 import React from "react";
 
 function CartPage() {
-  const { products, addToCart } = useCartStore();
+  const { products } = useCartStore();
+  return (
+    <>
+      <div className="container mx-auto bg-slate-200 rounded-md my-4 px-2 py-2">
+        <Headline text="Favorites" />
 
-  React.useEffect(() => {
-    console.log(products);
-  }, [products]);
-
-  return <div>CartPage</div>;
+        <div className="grid grid-cols-3 gap-2">
+          {products.map((product) => (
+            <ProductCart product={product} isCompact={false} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CartPage;
